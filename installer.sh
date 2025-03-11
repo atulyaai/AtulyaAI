f5b6a1fe2357291fd181f6826bf3a4f33fac201e
atulyaai <aj@atulvij.com>
2025-03-11 19:05:20 +0530

    Delete installer.sh

--- installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🔄 Installing AtulyaAI..."
-git clone https://github.com/atulyaai/AtulyaAI.git /opt/atulyaai
-cd /opt/atulyaai
-chmod +x install/install_system.py
-sudo python3 install/install_system.py
